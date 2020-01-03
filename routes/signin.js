420900f24d6a6fb0fb427575bd9001b1e72b660f
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-01-03 14:26:07 +0900

    function_login

--- routes/signin.js
+++ routes/signin.js
@@ -1,37 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var session = require('express-session');
+
 
 router.get('/', function(req, res, next) {
     console.log(req.user);
     res.render('login', {user:req.user});
 });
 router.post('/', passport.authenticate('local',
-  {successRedirect: '/tops',
-   failureRedirect: '/meet',
-   session: false}
-));
-
-// router.post('/signin',
-//   passport.authenticate('local',
-//     {
-//       failureRedirect: "/tops",
-//     }
-//   ),
-//   function(req, res,next){
-//     fetch("http://localhost:3000/signin",
-//    {
-//       credentials: "include"
-//    }
-//   ).then(function(){
-//    res.redirect("/meet");
-//   }).catch(function(e){
-//    console.log(e);
-//   });
-  // }
-// );
-
-
+    {successRedirect: '/',
+    failureRedirect: '/signin',
+    session: true}));
 
 
 module.exports = router;
