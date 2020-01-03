86a4c14f5c7a24eb037fb552fc714a048ec95289
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-01-03 14:26:29 +0900

    index_file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
 
 var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var bodyParser = require('body-parser');
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -17,18 +23,35 @@ connection.query('SELECT * from users;',function(err,rows,fields){
   if(err) throw err
 
   users = rows
+
+
 })
+// router.use(passport.initialize());
+// router.use(passport.session());
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // console.log(req.user)
+  console.log(req.user)
   // if(req.user){
-    res.render("index", {});
+    res.render("index", { user : req.user});
   // }else{
-  //   res.redirect("/signin");
-  }
+  //   res.render("index", {});
+  // }
+});
   // res.render('index', {user:req.user});
- 
-);
+  // router.get('/signin', function(req, res) {
+  //     res.render('login', { user : req.user });
+  // });
+
+  // router.post('/signin', passport.authenticate('local',
+  //     {successRedirect: '/',
+  //     failureRedirect: '/signin',
+  //     session: true}));
+
+  router.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
 //
 // router.get('/signin', function(req, res) {
 //   console.log(req.user);
