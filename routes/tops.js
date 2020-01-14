b53ef0d65d7d9ab2913398245127d8a454f20ddd
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-01-14 18:17:44 +0900

    Update tops.js

--- routes/tops.js
+++ routes/tops.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'myapp_part2',
+})
+
 router.get('/', function(req, res, next) {
-  res.send('ラッコだよ！');
+  console.log(req.user);
+
+      res.json(req.user);
+
 });
+
+// router.get('/', function(req, res, next) {
+//   console.log(req.user);
+//   connection.query("select * from users LIMIT 0, 10", function(error,users){
+//     if (error) throw error;
+//     connection.query("select * from likes LIMIT 0, 10", function(error,likes){
+//       if (error) throw error;
+//       likes.forEach(function(like){
+//         users.push(like)
+//         console.log(like);
+//       });
+//       var data = users
+//       res.json(data);
+//
+//     });
+//   });
+// });
 
 module.exports = router;
