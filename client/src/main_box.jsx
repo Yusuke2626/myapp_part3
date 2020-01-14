835bd1142bd605f5af8e10e54284588846b429ad
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-01-14 18:17:39 +0900

    Update main_box.jsx

--- client/src/main_box.jsx
+++ client/src/main_box.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {text:'signin',
-                  class:'users_box'};
-    // this.state = { count: 0};
+                  class:'users_box',
+                  sign_up_class:'',
+                  component_name:'',
+                  user:'nothing'
+                };
 
-    // fetch("http://localhost:3000/tops")
-    //   .then(response => response.json())
-    //   .then(users => this.setState({ users }));
+    fetch("http://localhost:3000/tops")
+      .then(response => response.json())
+      .then(user => this.setState({user}));
 
   }
 
   handleClick(name){
-    this.setState({class:name});
+    this.setState({component_name:name});
   }
 
 
   render(){
+    console.log(this.state.user);
+    const user = this.state.user
+    if (user !== 'nothing'){
+      return(
+        <div>
+          <div className={this.state.class}>
+            <h5 className='subtittle_sub2'>look for people</h5>
+          </div>
+          <div className ='index_list'>
+            <ul>
+              <li>
+                <a href='/meet'> share shareでroom share</a>
+              </li>
+              <li>
+                <a href='/'> share shareとは</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )
+    }else{
     return (
       <div>
         <div className={this.state.class}>
           <ul>
-            <li onClick={()=>{console.log('hello')}}>sign up</li>
+            <li onClick={()=>{this.handleClick('')}}>sign up</li>
             <li onClick={()=>{this.handleClick('sign_up_box')}}>{this.state.text}</li>
           </ul>
         </div>
@@ -40,27 +65,8 @@ class App extends React.Component{
         </div>
       </div>
     );
-}};
+    }
+  }
+};
 
 export default App;
-// .users_box
-//   ul
-//     if user === undifined
-//       li
-//         a(href='/users') sign up
-//       li
-//         a(href='/signin') log in
-//
-//   if user === undifined
-//       h2.subtittle_sub1
-//         a(href="/meet") look for people
-//   if user
-//     h5.subtittle_sub2
-//       a(href="/meet") look for people
-//
-// .index_list
-//   ul
-//     li
-//       a(href='/meet') share shareでroom share
-//     li
-//       a(href='/') share shareとは
