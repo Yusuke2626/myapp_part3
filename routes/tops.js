cd438493ef2429440d1196189f6295b4f55c177a
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-02-03 22:31:06 +0900

    Update tops.js

--- routes/tops.js
+++ routes/tops.js
@@ -12,8 +12,30 @@ var connection = mysql.createConnection({
 router.get('/', function(req, res, next) {
   console.log(req.user);
 
-      res.json(req.user);
+  connection.query(`select * FROM users where name LIKE '${req.user.username}';` , function(err,users){
+    console.log(users[0])
+    var user = users[0];
 
+    connection.query(`select * FROM userProfile1 where user_id = '${user.id}';` , function(err,profile){
+      var profile = profile[0];
+      console.log('user_profile', profile)
+
+      connection.query(`select * FROM users;` , function(err,users){
+        console.log(users);
+
+        var data = [];
+        data.push(user);
+        data.push(profile);
+        users.forEach(function(user){
+          data.push(user)
+          console.log(data);
+        });  
+
+        console.log('userdata', data);
+        res.json(data);
+      });
+    });
+  });
 });
 
 // router.get('/', function(req, res, next) {
