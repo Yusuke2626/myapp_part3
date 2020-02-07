8cc392589129479041febecb7967f1001c9f5f11
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-02-08 02:35:43 +0900

    add look for people

--- client/src/root.jsx
+++ client/src/root.jsx
@@ -11,7 +11,8 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {top_contentsOpen:true,
-                  myProfileOpen:false}
+                  myProfileOpen:false,
+                  lookForPeopleOpen:false}
 
     fetch("http://localhost:3000/tops",{mode: 'cors'})
       .then(response => response.json())
@@ -24,7 +25,9 @@ export default class App extends React.Component {
   handleClick(){
     this.setState({myProfileOpen:true})
   }
-
+  handleClickLookForPeople(){
+    this.setState({lookForPeopleOpen:true,myProfileOpen:false})
+  }
   render() {
     var user = this.state.user;
     var cursorstyle = {cursor:'pointer'};
@@ -49,13 +52,13 @@ export default class App extends React.Component {
       return(
         <div>
           <h3>share shareでルームメイトを見つけよう </h3>
-          <h4 style={cursorstyle} onClick={()=>{this.handleClick()}}> my_profile</h4>
+          <h4 style={cursorstyle} onClick={()=>{this.handleClickLookForPeople()}}> look for people</h4>
           <div className='contents2' >
             < MyProfile />
           </div>
         </div>
     );
-    }else{
+  }else if(this.state.lookForPeopleOpen){
 
       console.log('root_login_user',user[0]);
       console.log('root_login_user_name',user[0].name);
@@ -115,5 +118,65 @@ export default class App extends React.Component {
         </div>
       );
     }
+    else{
+
+        console.log('root_login_user',user[0]);
+        console.log('root_login_user_name',user[0].name);
+        console.log('root_login_user_profile_area',user[1].area);
+        console.log('all_user',user);
+
+        var users = user
+        var profile = user[1];
+        var user = user[0];
+
+        var users = users.filter(n => n !== users[0] && n!==users[1]);
+        var users = users.filter(n => n.id !== user.id)
+        var num = 0;
+        var index_num = 999;
+        var partner_block = [];
+
+        for(var u of users){var current_num = index_num - num;
+                            partner_block.push(
+                            <div className='panel_partner' style={{zIndex:{current_num}}}>
+                              <p> Name: {u.name}</p>
+                              <div className='image_area'>
+                                <img src={u.img}/>
+                              </div>
+                              <div className='prof_list'>
+                                <p className='list'> よろしく </p>
+                                <p className='list'> 旅行 </p>
+                                <p className='list'> ▽ </p>
+                              </div>
+                              <div className='btn_area'>
+                              </div>
+                            </div>)
+                            var num = num+1;}
+
+        return(
+          <div>
+            <h3>share shareでルームメイトを見つけよう </h3>
+            <h4 style={cursorstyle} onClick={()=>{this.handleClick()}}> my_profile</h4>
+            <div className='contents2' >
+              <div className='self_area'>
+                <div className='panel_self'>
+                  <p> your profile</p>
+                  <p>Name:{user.name}</p>
+
+                  <div className='image_area_self'>
+                    <img className='my_img' src={user.img}/>
+                  </div>
+                  <p> area:{profile.area}</p>
+                </div>
+              </div>
+              <div className='partner_area'>
+              {partner_block}
+                <div className='add_cards'>
+                  <p>No User</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
   }
 }
