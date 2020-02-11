61a3e88be384f029dd46feb55b6c8a58a0bb03f6
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2020-02-11 18:32:49 +0900

    Update message.jsx

--- client/src/message.jsx
+++ client/src/message.jsx
@@ -5,13 +5,38 @@ import './App.css';
 class App extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {data:'nothing'};
+    this.childRef = React.createRef();
+    this.state = {data:'nothing',
+                  likeUserId:0,
+                  value:0,
+                  text:'テキストを入力',
+                  userId:0};
 
     fetch("http://localhost:3000/mypage")
       .then(response => response.json())
       .then(data => this.setState({data}));
     }
 
+
+    postMessage(){
+
+      fetch('http://localhost:3000/partner',{
+        method: 'POST',
+        headers:{
+          Accept: 'application/json',
+          'Content-Type':'application/json',
+        },
+         body: JSON.stringify({
+          text:this.state.text,
+          user_id:this.state.userId,
+        }),
+      },
+      fetch("http://localhost:3000/mypage")
+        .then(response => response.json())
+        .then(data => this.setState({data}))
+      )
+      console.log('ok');
+    };
 
 
   render(){
@@ -22,6 +47,22 @@ class App extends React.Component{
     console.log(meet_people);
     console.log('myProfile',profile);
     console.log('myprofile_user',user);
+    console.log('value',this.props.likeUserId);
+    console.log('alldata',this.state.data[2]);
+    var partnerUserId = this.props.likeUserId;
+    var likeUsers = this.state.data[2];
+    console.log(likeUsers);
+    console.log('likeUsers.length',likeUsers.length);
+
+    // var partnerUser = likeUsers[0].filter(u=>u.id ===partnerUserId)
+    if(likeUsers.length>1){
+      var partnerUser = likeUsers.filter(u=>u.id ===partnerUserId)[0]
+    }else{
+      var partnerUser = likeUsers[0]
+    }
+
+    console.log('partnerUser',partnerUser);
+    // .find((p)=>p.user_id ===u.id);
 
     var meet_people_block = [];
     for(var u of meet_people){
@@ -33,15 +74,15 @@ class App extends React.Component{
     return(
     <div>
       <div className='partner_profile_area' >
-        <p>partner_name</p>
-        <img className='people_img' src=''/>
+        <p>{partnerUser.name}</p>
+        <img className='people_img' src={partnerUser.img}/>
         <p>profile.area</p>
       </div>
       <div className='message_area'>
         <p>each message</p>
-        <input className='message_input'/>
       </div>
-
+      <input type="text"  className='message_input' name='text' value={this.state.text} onChange={(e) => this.setState({text: e.target.value,userId:user.id})}/>
+      <input type='submit' value='送信' className='message_submit' style={{margin: 5,width:200}} onClick={()=>{this.postMessage()}}/>
     </div>
     )
   }
